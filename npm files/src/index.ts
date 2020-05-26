9d896ab116665e4dd1c3db8b511c52d3ef97ff9a
Jannick Pedersen <59564388+japnovicell@users.noreply.github.com>
2020-05-26 14:21:01 +0200

    Add files via upload

---
+++ npm files/src/index.ts
@@ -0,0 +1,21 @@
+import NovicellButton from './click-button';
+import TestButton from './link-button';
+import DatePicker from './date-picker';
+import OsChecker from './os-checker';
+import waves from './wave';
+import intersectionobserver from './intersectionobserver';
+import backgroundpicker from './backgroundpicker';
+import themepicker from './theme-picker';
+import textBackgroundDarkmodePicker from './text-background-darkmode-picker';
+
+export {
+    NovicellButton,
+    TestButton,
+    DatePicker,
+    OsChecker,
+    waves,
+    intersectionobserver,
+    backgroundpicker,
+    themepicker,
+    textBackgroundDarkmodePicker,
+}
